7b13ab4d4e1fbf73bd2d1812efa98f4457892e8b
Evan Mitchell <evan.m.mitchell@gmail.com>
2019-05-28 16:00:30 -0700

    Use OrthographicCamera

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,38 @@
-var scene = new THREE.Scene();
-// var camera = new THREE.OrthographicCamera(window.innerWidth / - 2,
-//                       window.innerWidth / 2, window.innerHeight / 2,
-//                       window.innerHeight / - 2, 1, 1000);
-var canvas = document.getElementById("myCanvas");
-var renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+var scene, camera, renderer;
+var cube;
 
-var camera = new THREE.PerspectiveCamera(75,
-                      canvas.offsetWidth / canvas.offsetHeight, 0.1, 1000);
+init();
+animate();
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: false});
-var cube = new THREE.Mesh(geometry, material);
 
-scene.add(cube);
-camera.position.z = 3;
+function init() {
+  var canvas = document.getElementById("myCanvas");
 
-function update() {
+  scene = new THREE.Scene();
 
+  var frustumSize = 3;
+  var aspect = canvas.offsetWidth / canvas.offsetHeight;
+  camera = new THREE.OrthographicCamera(frustumSize * aspect / -2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / -2, 1, 1000);
+  // camera = new THREE.PerspectiveCamera(75, canvas.offsetWidth / canvas.offsetHeight, 0.1, 1000);
+  camera.position.set(0, 0, 3);
+
+  renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 }
 
 function render() {
+  cube.rotation.x += 0.002;
+  cube.rotation.y += 0.001;
+
   renderer.render(scene, camera);
 }
 
-function GameLoop() {
-  requestAnimationFrame(GameLoop);
-  update();
+function animate() {
+  requestAnimationFrame(animate);
   render();
 }
-
-GameLoop();
