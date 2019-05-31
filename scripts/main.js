727fcf0e88b9ca12d3bfc945724071e3b0224470
Evan Mitchell <evan.m.mitchell@gmail.com>
2019-05-30 21:42:30 -0700

    Add song title and box scaling

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,56 @@
 let scene, camera, renderer;
+let xScale, yScale, zScale;
 let notes = [];
-// let blocks = [];
-// let i = 0;
 
-getNotes();
-init();
+initialize();
 animate();
 
 
-function getNotes() {
+function getMusic() {
+  let title = document.getElementById("title");
+  title.textContent = "Sample Music";
+
   let sourceNotes = [{ pitch: 1, start: 0, duration: 2 }, { pitch: 0, start: 2, duration: 1 }];
   for (let i = 0; i < sourceNotes.length; i++) {
     let note = sourceNotes[i];
     notes.push(note);
   }
+
+  xScale = 1;
+  yScale = 0.25;
+  zScale = 0.25;
 }
 
-function init() {
+function initialize() {
+  getMusic();
+
   let canvas = document.getElementById("myCanvas");
 
   scene = new THREE.Scene();
 
   let frustumSize = 10;
-  let aspect = canvas.offsetWidth / canvas.offsetHeight;
-  camera = new THREE.OrthographicCamera(frustumSize * aspect / -2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / -2, 1, 1000);
+  let aspectRatio = canvas.offsetWidth / canvas.offsetHeight;
+  camera = new THREE.OrthographicCamera(frustumSize * aspectRatio / -2, frustumSize * aspectRatio / 2, frustumSize / 2, frustumSize / -2, 1, 1000);
   camera.position.set(0, 0, 3);
 
   renderer = new THREE.WebGLRenderer({ canvas });
   renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 
-  // For static visualization
+  // Static visualization
   for (let i = 0; i < notes.length; i++) {
     let note = notes[i];
-    let geometry = new THREE.BoxGeometry(note.duration, 1, 1);
+    let geometry = new THREE.BoxGeometry(note.duration * xScale, yScale, zScale);
     let material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, wireframe: true });
     let block = new THREE.Mesh(geometry, material);
-    block.position.set(note.start + (note.duration) / 2, note.pitch, 0);
+    block.position.set((note.start + note.duration / 2) * xScale, note.pitch * yScale, 0);
     scene.add(block);
   }
 }
 
-function render() {
-  // For dynamic visualization
-  // if (i < notes.length) {
-  //   block.geometry = new THREE.BoxGeometry(i * 0.1, 1, 1);
-  //   block.position.x = i * 0.05;
-  //
-  //   i++;
-  // }
-
-  renderer.render(scene, camera);
-}
-
 function animate() {
   requestAnimationFrame(animate);
-  render();
+
+  // use THREE.Clock for time in dynamic visualization
+
+  renderer.render(scene, camera);
 }
